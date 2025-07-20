94fbd0815afc784034ec3ecaa5e180aaa2a3ff71
guardianprime <okongordian@gmail.com>
2025-07-20 19:15:12 +0100

    feat: created the login and signup pages

--- routes/shorten.js
+++ routes/shorten.js
@@ -1,4 +1,5 @@
 const shortenRouter = require("express").Router();
+const { urlModel } = require("../models/urlModel");
 
 shortenRouter.post("/", (req, res) => {
   const { url } = req.body;
@@ -6,9 +7,17 @@ shortenRouter.post("/", (req, res) => {
   const shortenedUrl = `http://short.url/${Math.random()
     .toString(36)
     .substring(7)}`;
+  urlModel.create({ originalUrl: url, shortenedUrl });
   res.render("index", { shortenedUrl, error: null });
   console.log(`Shortened URL: ${shortenedUrl}`);
   console.log(`Original URL: ${url}`);
+});
+
+shortenRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+  // Here you would typically retrieve the original URL using the ID
+  const originalUrl = urlModel.findById(id);
+  res.render("index", { shortenedUrl: null, error: null, originalUrl });
 });
 
 module.exports = shortenRouter;
