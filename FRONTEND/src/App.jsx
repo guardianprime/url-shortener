620f215b581ef3778fb7fc082537135d40090737
guardianprime <okongordian@gmail.com>
2025-07-31 21:33:33 +0100

    chore: created a hamburger menu

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import Footer from "./components/Footer";
+import Hamburger from "./components/Hamburger";
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      {/* //<Navbar /> */}
+      <Hamburger />
       <HeroSection />
       <Footer />
     </>
