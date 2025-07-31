ea07ff237de6ad48d1f12145f0dc07e633b303c3
guardianprime <okongordian@gmail.com>
2025-07-31 08:48:01 +0100

    navbar content

---
+++ FRONTEND/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+const Navbar = () => {
+  return (
+    <header>
+      <div>logo image</div>
+      <nav>
+        <ul>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+        </ul>
+        <button>sign in</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
