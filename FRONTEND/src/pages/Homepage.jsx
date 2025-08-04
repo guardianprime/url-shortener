0b814fc260ba181dcdff084b0e2bde147788258b
guardianprime <okongordian@gmail.com>
2025-08-05 00:28:12 +0100

    fix: the homepage not displaying issues

--- FRONTEND/src/pages/Homepage.jsx
+++ FRONTEND/src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import Footer from "../components/Footer";
 import Hamburger from "../components/Hamburger";
+import HeroSection from "../components/HeroSection";
 
 function Homepage() {
   return (
